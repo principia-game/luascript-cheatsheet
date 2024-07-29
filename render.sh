6f73389938161ae7a2bad843b5a803a8a44a817c
ROllerozxa <rollerozxa@voxelmanip.se>
2024-07-29 23:46:07 +0200

    Add black-and-white version for greyscale printers

---
+++ render.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+weasyprint cheatsheet.html cheatsheet.pdf
+
+okular cheatsheet.pdf &
+
+declare -A replacements
+replacements=(
+	["icon-callbacks.svg"]="icon-callbacks_w.svg"
+	["icon-cam.svg"]="icon-cam_w.svg"
+	["icon-creature.svg"]="icon-creature_w.svg"
+	["icon-entity.svg"]="icon-entity_w.svg"
+	["icon-game.svg"]="icon-game_w.svg"
+	["icon-this.svg"]="icon-this_w.svg"
+	["icon-world.svg"]="icon-world_w.svg"
+	["lualogo.svg"]="lualogo_bw.svg"
+	["principia.png"]="principia_bw.png"
+)
+
+sed_expr=""
+for old in "${!replacements[@]}"; do
+    new=${replacements[$old]}
+    sed_expr+="s/$old/$new/g;"
+done
+
+sed -e "$sed_expr" cheatsheet.html > cheatsheet_grey.html
+
+weasyprint cheatsheet_grey.html cheatsheet_grey.pdf -s style_grey.css
+
+okular cheatsheet_grey.pdf &
